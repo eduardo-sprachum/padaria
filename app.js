68c77cd24950ae850d89ae291379a8b4715a5a92
eduardo-sprachum <145134393+eduardo-sprachum@users.noreply.github.com>
2025-05-20 15:07:55 -0300

    .

--- app.js
+++ app.js
@@ -2,9 +2,5 @@ document.getElementById("btnVendas").addEventListener("click", function() {
     let vendasSection = document.getElementById("vendas");
 
     // Alterna entre mostrar e esconder a seção de vendas
-    if (vendasSection.style.display === "none" || vendasSection.style.display === "") {
-        vendasSection.style.display = "block";
-    } else {
-        vendasSection.style.display = "none";
-    }
+    vendasSection.style.display = (vendasSection.style.display === "none" || vendasSection.style.display === "") ? "block" : "none";
 });
